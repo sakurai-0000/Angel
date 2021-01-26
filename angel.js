8c4ffa0593b756a64c0f6c38fc22f273626566f8
sakurai-0000 <sr.macnica19910603@icloud.com>
2021-01-27 00:42:21 +0900

    db.js作成, get→post, リファクタリング

--- angel.js
+++ angel.js
@@ -5,18 +5,25 @@ const AWS = require('aws-sdk');
 
 // 外部関数
 const handler = require('./handler');
+const db = require('./db');
 
-module.exports.angel = async event => {
+module.exports.angel = async req => {
+  const event = process.env.LOCAL
+    ? JSON.parse(req.body)
+    : req.queryStringParameters;
+
+  console.log(`req: ${event}`);
   try {
     // パラメータチェック
-    if (!await handler.paraCheck(event.queryStringParameters)) {
+    if (!await handler.paraCheck(event)) {
       throw new Error('Faile to get Input Parameters');
     };
     // 対象ユーザーデータ取得
-    const userInfo = await handler.get(event.queryStringParameters);
+    const userInfo = await db.get(event);
     if (userInfo.status === 'failed') {
       throw new Error();
     }
+
     // Angel Challenge
     const angelRes = handler.angelChallenge();
 
@@ -25,27 +32,30 @@ module.exports.angel = async event => {
 
     // update
     const changeTableRes = !userInfo.result
-      ? await handler.put(event.queryStringParameters, angelRes)
-      : await handler.update(userInfo, angelRes, bonusRes);
+      ? await db.put(event, angelRes)
+      : await db.update(userInfo, angelRes, bonusRes);
     if (changeTableRes.status === 'failed') {
       throw new Error(changeTableRes.result);
     }
-
-    const res = {
-      mail_adress: event.queryStringParameters.mail_adress,
-      isAngel: angelRes.res,
-      isBonus: bonusRes.res,
-    };
     return {
-      status: 'successed',
-      result: JSON.stringify(res),
+      body: JSON.stringify(
+        {
+          status: 'successed',
+          result: {
+            id: event.id,
+            mail_adress: event.mail_adress,
+            isAngel: angelRes.res,
+            isBonus: bonusRes.res,
+          },
+        }),
     };
   } catch (error) {
     console.log(error);
     return {
-      result: 'failed',
-      message: error.message,
-      body: JSON.stringify({ message: error.message }),
+      body: JSON.stringify({
+        result: 'failed',
+        message: error.message
+      }),
     };
   }
 
